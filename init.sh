e7f0a50a12e33d2014a2d06f2bfc0e2865b0ec34
teodord25 <djuric.teodor25@gmail.com>
2022-12-01 22:42:18 +0100

    add init script

---
+++ init.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [[ $# -ne 2 ]] ; then 
+  echo "Must supply 2 arguments"
+  exit 1
+fi
+
+day=$1
+name=$2
+mkdir $day && cd $day
+
+mkdir Python && cd Python && mkdir $name
+echo "def main():
+    pass
+
+if __name__ == \"__main__\":
+    main()" >> $name/main.py
+
+cd ..
+mkdir Rust && cd Rust && cargo new $name
+
