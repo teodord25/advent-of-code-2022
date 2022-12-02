e1a1c7f1d118fe902cb13e62892b0cc80a42436a
teodord25 <djuric.teodor25@gmail.com>
2022-12-02 13:16:08 +0100

    Rust day_1 both parts

--- day_1/Rust/calorie_counting/src/main.rs
+++ day_1/Rust/calorie_counting/src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+fn part_1(input_str: &str) -> i64 {
+    let mut sums =  input_str
+        .split("\n\n")
+        .map(
+            |elf| elf
+            .split("\n")
+            .map(|snack| snack
+                 .parse::<i64>()
+                 .unwrap())
+            .sum()
+        ).collect::<Vec<i64>>();
+
+    sums.sort();
+    *sums.last().unwrap()
 }
+
+fn part_2(input_str: &str) -> i64 {
+    let mut sums =  input_str
+        .split("\n\n")
+        .map(
+            |elf| elf
+            .split("\n")
+            .map(|snack| snack
+                 .parse::<i64>()
+                 .unwrap())
+            .sum()
+        ).collect::<Vec<i64>>();
+
+    sums.sort();
+    sums[sums.len() - 3 ..].iter().sum()
+}
+
+
+fn main() {
+    // println!("{}", part_1(&input_str));
+    // println!("{}", part_2(&input_str));
+}
+
