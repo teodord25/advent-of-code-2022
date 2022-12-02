abac82923e7c0b76a08f4321db190215665edae3
teodord25 <djuric.teodor25@gmail.com>
2022-12-03 00:31:31 +0100

    day 2 part 2 Rust goofy solution

--- day_2/Rust/rock_paper_scissors/src/main.rs
+++ day_2/Rust/rock_paper_scissors/src/main.rs
@@ -45,11 +45,91 @@ fn part_1(input_str: &str) -> i64 {
 }
 
 fn part_2(input_str: &str) -> i64 {
-    0
+
+    /*
+     * X -> pick losing shape
+     * Y -> pick draw
+     * Z -> pick win
+     */
+
+    let mut total = 0;
+    let pairs = input_str.split("\n");
+    for pair in pairs {
+        let opponent_shape = pair.split_whitespace().collect::<Vec<&str>>()[0];
+        let my_result = pair.split_whitespace().collect::<Vec<&str>>()[1];
+
+        let mut my_shape = "";
+        let mut game_result = 0;
+        
+        if opponent_shape == "A" {
+            if my_result == "X" {
+                game_result = 0;
+                my_shape = "S";
+            }
+
+            if my_result == "Y" {
+                game_result = 3;
+                my_shape = "R";
+            }
+
+            if my_result == "Z" {
+                game_result = 6;
+                my_shape = "P";
+            }
+
+        }
+
+        if opponent_shape == "B" {
+            if my_result == "X" {
+                game_result = 0;
+                my_shape = "R";
+            }
+
+            if my_result == "Y" {
+                game_result = 3;
+                my_shape = "P";
+            }
+
+            if my_result == "Z" {
+                game_result = 6;
+                my_shape = "S";
+            }
+
+        }
+
+        if opponent_shape == "C" {
+            if my_result == "X" {
+                game_result = 0;
+                my_shape = "P";
+            }
+
+            if my_result == "Y" {
+                game_result = 3;
+                my_shape = "S";
+            }
+
+            if my_result == "Z" {
+                game_result = 6;
+                my_shape = "R";
+            }
+
+        }
+
+        let shape_value = match my_shape {
+            "R" => 1,
+            "P" => 2,
+            "S" => 3,
+            _ => 0,
+        };
+
+
+        total += shape_value;
+        total += game_result;
+    }
+    total
 }
 
 fn main() {
     println!("{}", part_1(&input_str));
-    part_2(&input_str);
-
+    println!("{}", part_2(&input_str));
 }
