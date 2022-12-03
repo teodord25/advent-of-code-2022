eb4336d6e824d1d2038fbd2c0ef71c46e3e01242
teodord25 <djuric.teodor25@gmail.com>
2022-12-03 19:48:32 +0100

    python d3p2

--- day_3/Python/rucksack_reorganization/main.py
+++ day_3/Python/rucksack_reorganization/main.py
@@ -1,3 +1,6 @@
+import re
+
+
 def part_1(input_str):
     total = 0
     rucksacks = input_str.split("\n")
@@ -14,11 +17,32 @@ def part_1(input_str):
 
         total += ord(char) - 96 if char.islower() else ord(char) - 64 + 26
 
-    print(total)
+    return total
+
+
+def part_2(input_str):
+    total = 0
+
+    # split at every third occurrence of \n
+    rucksacks = re.findall("\n".join(["[^\n]+"] * 3), input_str)
+
+    char = ""
+    for triple in rucksacks:
+        ruck_1, ruck_2, ruck_3 = triple.split("\n")
+        for ch in ruck_1:
+            if ch in ruck_2:
+                if ch in ruck_3:
+                    char = ch
+
+
+        total += ord(char) - 96 if char.islower() else ord(char) - 64 + 26
+    return total
+    
 
 
 def main(input_str):
-    part_1(input_str)
+    print(part_1(input_str))
+    print(part_2(input_str))
 
 
 if __name__ == "__main__":
