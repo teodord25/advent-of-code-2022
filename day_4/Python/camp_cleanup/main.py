3b344033107297c859026afe0898b180687e7935
teodord25 <djuric.teodor25@gmail.com>
2022-12-04 18:51:25 +0100

    d4p2 python

--- day_4/Python/camp_cleanup/main.py
+++ day_4/Python/camp_cleanup/main.py
@@ -1,4 +1,4 @@
-def main(input_str):
+def part_1(input_str):
     pairs = input_str.split("\n")
 
     total = 0
@@ -11,7 +11,25 @@ def main(input_str):
         elif right[0] <= left[0] and right[1] >= left[1]:
             total += 1
 
-    print(total)
+    return total
+
+
+def part_2(input_str):
+    pairs = input_str.split("\n")
+
+    total = 0
+    for pair in pairs:
+        left, right = [[int(s) for s in i] for i in [j.split("-") for j in pair.split(",")]]
+
+        if not (left[1] < right[0] or left[0] > right[1]):
+            total += 1
+
+    return total
+
+
+def main(input_str):
+    print(part_1(input_str))
+    print(part_2(input_str))
 
 if __name__ == "__main__":
     main(input_str)
