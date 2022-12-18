75e987cc0878e99cfdd266f151f4a80b84bfe458
teodord25 <djuric.teodor25@gmail.com>
2022-12-18 02:33:50 +0100

    rust day 10 part 2 (it's off by 1 but I don't wanna fix it)

--- 10/Rust/src/main.rs
+++ 10/Rust/src/main.rs
@@ -41,11 +41,77 @@ fn part_1(input_str: &str) -> i32 {
 }
 
 fn part_2(input_str: &str) -> i64 {
-    let total = 0;
+    let mut register = 1;
+
+    let mut current_cycle = 1;
+
+    let lines: Vec<&str> = input_str.trim().split("\n").collect();
+    let mut second_cycle = false;
+
+    let mut i = 0;
+    while i < lines.len() {
+        let line = lines[i];
+        let tokens: Vec<&str> = line.split(" ").collect();
+        let instruction = tokens[0];
+
+        if second_cycle {
+            second_cycle = false;
+            register += tokens[1].parse::<i32>().unwrap();
+            i += 1;
+            
+
+            if current_cycle % 40 == register - 1
+                || current_cycle % 40 == register ||
+                    current_cycle % 40 == register + 1 {
+                        print!("#");
+                    }
+            else {
+                print!(".");
+            }
 
 
+            if current_cycle % 40 == 0 {print!("\n");}
 
-    total
+            current_cycle += 1;
+
+            continue;
+        }
+
+        if instruction == "addx" {
+            second_cycle = true;
+
+
+            if current_cycle % 40 == register - 1
+                || current_cycle % 40 == register ||
+                    current_cycle % 40 == register + 1 {
+                        print!("#");
+                    }
+            else {
+                print!(".");
+            }
+            if current_cycle % 40 == 0 {print!("\n");}
+
+
+            current_cycle += 1;
+
+        } else {
+            i += 1;
+
+            if current_cycle % 40 == register - 1
+                || current_cycle  % 40 == register ||
+                    current_cycle % 40 == register + 1 {
+                        print!("#");
+                    }
+            else {
+                print!(".");
+            }
+            if current_cycle % 40 == 0 {print!("\n");}
+
+            current_cycle += 1;
+
+        }
+    }
+    0
 }
 
 
@@ -55,6 +121,6 @@ fn main() {
     if input_str.trim().len() == 0 {
         panic!("puzzle input string missing"); 
     }
-    println!("{}", part_1(&input_str));
+    // println!("{}", part_1(&input_str));
     println!("{}", part_2(&input_str));
 }
