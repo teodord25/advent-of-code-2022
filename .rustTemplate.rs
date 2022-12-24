473e65706c9a756ce0f8a6cc04c7ca2a06679b1a
Teodor Đurić <70525484+teodord25@users.noreply.github.com>
2022-12-24 23:20:29 +0100

    Update .rustTemplate.rs

--- .rustTemplate.rs
+++ .rustTemplate.rs
@@ -1,13 +1,15 @@
 use std::fs;
 
 
-fn code(input_str: &str) -> i64 {
+fn part_1(input_str: &str) {
+}
 
+fn part_2(input_str: &str) {
 }
 
 
 fn main() {
-    let input_str: String = String::from(fs::read_to_string("input.txt").expect("Unable to read input file."));
+    let input_str: String = String::from(fs::read_to_string("input.txt").expect("Unable to read input file.")).trim();
 
     if input_str.trim().len() == 0 {
         panic!("puzzle input string missing"); 
